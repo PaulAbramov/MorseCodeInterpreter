8444ce2c05fa872c4ff3714b50792dae3b1d4d99
Paul Abramov <paul.abramov1911@gmail.com>
2022-08-24 00:22:15 +0200

    Add Base as a reference

---
+++ Base/Program.cs
@@ -0,0 +1,19 @@
+﻿// Your task is to write a simple morse encoder/decoder.
+// You are allowed to lookup morse code on wikipedia.
+
+// To separate letters from each other a single space is used.
+// To separate words from one another three space characters are used.
+
+// A map of morse code to characters exists in the MorseAlphabet.cs file.
+
+string toBeDecoded = "-.. . -.-. --- -.. .   -- .   --..-.   .--. .-. --- .--. . .-. .-.. -.-- -.-.--";
+string toBeEncoded = "Encode me - properly!";
+
+// Print the decoded morse code to the console.
+Console.WriteLine();
+
+// Print the encoded morse code to the console.
+Console.WriteLine();
+
+
+Console.ReadKey();
